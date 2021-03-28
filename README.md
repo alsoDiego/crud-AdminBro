0cd68fc25c2145b76c074b47bfa95e542b08734c
Diego Soares <as.diego@live.com>
2021-03-28 17:53:21 -0300

    :tada: complete application

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Install dependencies
 npm i admin-bro @admin-bro/express express express formidable
+npm i @admin-bro/mongoose mongoose
---
+++ admin.js
@@ -0,0 +1,74 @@
+//============================================
+//=============== Database ===================
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    completed: Boolean,
+    created_at: { type: Date, default: Date.now },
+});
+
+const Project = mongoose.model("Project", ProjectSchema);
+
+//============================================
+//=============== AdminBro ===================
+const AdminBro = require('admin-bro');
+const AdminBroExpress = require('@admin-bro/express');
+const AdminBroMongoose = require("@admin-bro/mongoose");
+
+// use Mongose in AdminBro
+AdminBro.registerAdapter(AdminBroMongoose);
+
+// config
+const adminBroOptions = new AdminBro({
+    resources: [
+        {
+            resource: Project,
+            options: {
+                properties: {
+                    description: { type: 'richtext' },
+                    created_at: {
+                        invisible: { edit: false, list: true, show: true, filter: true }
+                    }
+                }
+            }
+        },
+    ],
+
+    
+    locale: {
+        translations: {
+            labels: {
+                Project: 'Meus projetos'
+            }
+        }
+    },
+
+    rootPath: '/admin'
+});
+
+const router = AdminBroExpress.buildRouter(adminBroOptions);
+
+//============================================
+//================ Server ====================
+const express = require("express");
+const server = express();
+
+server.use(adminBroOptions.options.rootPath, router)
+
+//============================================
+//================ Run App ===================
+const run = async () => {
+    await mongoose.connect("mongodb://localhost/adminbroapp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    await server.listen(5500, () => console.log("Server started"));
+};
+
+run();
